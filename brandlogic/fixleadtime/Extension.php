a9b81ebb2a80065861a47d1d12a25528531889bd
Rozendaal Multimedia <info@rozendaalmultimedia.nl>
2022-12-07 17:31:10 +0100

    Initial commit

---
+++ brandlogic/fixleadtime/Extension.php
@@ -0,0 +1,26 @@
+<?php namespace Thoughtco\ChooseTime;
+
+use Event;
+use Igniter\Flame\Exception\ApplicationException;
+use System\Classes\BaseExtension;
+use Igniter\Local\Facades\Location;
+use Carbon\Carbon;
+
+/**
+ * Fix LeadTime Extension Information File
+ */
+class Extension extends BaseExtension
+{
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return void
+     */
+    public function boot()
+    {   
+        Event::listen('igniter.workingSchedule.timeslotValid', function ($workingSchedule, $timeslot) {
+            $orderLeadTime = Location::current()->getOption($workingSchedule->getType() . '_lead_time');
+            return !(Carbon::now()->diffInMinutes($timeslot) < $orderLeadTime);
+        });
+    }
+}
---
+++ brandlogic/fixleadtime/README.md
@@ -0,0 +1,8 @@
+Fix leadtime fixes the issue that no leadtime is added once ASAP delivery/pick-up is turned off. 
+
+### Installation
+Upload to extensions in the following folder format:
+
+`extensions/brandlogic/fixleadtime/`
+
+In the admin user interface enable the extension.
